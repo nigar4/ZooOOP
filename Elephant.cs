4f8ca7383ca44bf4ea551a44872b0aaf09005cb6
nigar4 <nigar-djafarova@mail.ru>
2022-11-01 16:57:59 +0400

    Add project files.

---
+++ Elephant.cs
@@ -0,0 +1,16 @@
+﻿namespace OOP
+{
+    public class ElephantAnimal : AbstractAnimal
+    {
+
+        public ElephantAnimal(string name, int age) : base(name, age)
+        {
+            Biom = BiomType.Tropic;
+            Eat = "trava";
+            _sound = "UUUUU";
+            Name = "Lili";
+        }
+
+         
+    }
+}
